6aaa28061ef5980b358c10020fbb2b44fedf460f
tykord <brianwc@sunnyoasis.com>
2021-07-08 23:16:44 -0400

    Executable Creation

--- Inventory.ps1
+++ Inventory.ps1
@@ -2,28 +2,26 @@ $BIOS = Get-WmiObject -Class Win32_Bios
 $OS = Get-WmiObject -class Win32_OperatingSystem
 $PC = Get-WMIObject -class Win32_ComputerSystem
 $RAM = [math]::Round((($PC.TotalPhysicalMemory/1024)/1024)/1024)
+[string]$Random = 'X^#1kd950ajRGHzvnWMqlD$Pd*DBW9HBU*fJ7pZ6yOxX8otmQ&aV9xT'
 $CPU = Get-WmiObject -Class Win32_Processor
-
-Write-Host "User Name:          ",$PC.UserName
-Write-Host "Computer Name:      ",$OS.PSComputerName
-Write-Host "Computer Make:      ",$PC.Manufacturer
-Write-Host "Computer Model:     ",$PC.Model
-Write-Host "Computer SN:        ", $BIOS.SerialNumber
-Write-Host "CPU Installed:      ",$CPU.Name
-Write-Host "CPU Core Count:     ",$CPU.NumberOfCores,"cores"
-Write-Host "Installed RAM:      ",$RAM,"GB"
-Write-Host "BIOS Release Date:  ",($BIOS.ConvertToDateTime($BIOS.releasedate).ToShortDateString())
-Write-Host "OS Install Date:    ",($OS.ConvertToDateTime($OS.InstallDate).ToShortDateString())
-Write-Host "OS Version:         ", $OS.Caption
-Write-Host "OS Version:         ", $OS.Version
-
-
-# Define Credentials
-[string]$userName = 'brianwc@miltown.com'
-[string]$userPassword = 'here'
-
-# Crete credential Object
-[SecureString]$secureString = $userPassword | ConvertTo-SecureString -AsPlainText -Force 
-[PSCredential]$credentialObejct = New-Object System.Management.Automation.PSCredential -ArgumentList $userName, $secureString
-
-Send-MailMessage -From brianwc@miltown.com -to brianwc@miltown.com -subject "Inventory - BRCGS" -Body "Forgot to send the attachment. Sending now." -Attachments C:\DATA\GitHub\BRCGS\Inventory.ps1 -SmtpServer 'smtp.office365.com' -UseSsl -Credential $credentialObejct
+$CPUCore = $CPU.NumberOfCores
+$CSVItems = [PSCustomObject]@{
+    UserName = $PC.UserName;
+    ComputerName = $OS.PSComputerName;
+    ComputerMake = $PC.Manufacturer;
+    ComputerModel = $PC.Model;
+    ComputerSN =  $BIOS.SerialNumber;
+    CPUInstalled = $CPU.Name;
+    CPUCoreCount = $CPUCore;
+    InstalledRAM = $RAM;
+    BIOSReleaseDate = ($BIOS.ConvertToDateTime($BIOS.releasedate).ToShortDateString());
+    OSInstallDate= ($OS.ConvertToDateTime($OS.InstallDate).ToShortDateString());
+    OSVersion =  $OS.Caption;
+    OSVer =  $OS.Version
+}
+$CSVItems | Export-Csv -Path C:\temp\TestObject.csv
+[string]$email = 'inventory@tykord.com'
+$Subject = "Inventory - " + $PC.username
+[SecureString]$secureString = $Random | ConvertTo-SecureString -AsPlainText -Force 
+[PSCredential]$credentialObejct = New-Object System.Management.Automation.PSCredential -ArgumentList $email, $secureString
+Send-MailMessage -From inventory@tykord.com -to brianwc@miltown.com -subject $Subject -Body "Forgot to send the attachment. Sending now." -Attachments C:\temp\TestObject.csv -SmtpServer 'smtp.office365.com' -UseSsl -Credential $credentialObejct
